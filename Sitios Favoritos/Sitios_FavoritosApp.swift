31b1928f91b3ed62e1961202a51f1daa359a86a2
hernirg <hernanrg85@gmail.com>
2024-08-03 19:10:16 +0200

    Initial Commit

---
+++ Sitios Favoritos/Sitios_FavoritosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sitios_FavoritosApp.swift
+//  Sitios Favoritos
+//
+//  Created by Hernán Rodríguez on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Sitios_FavoritosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
